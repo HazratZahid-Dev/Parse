d25453716ce12dd7d5b220022a297e350076133a
HazratZahid9454 <zahid454688@gmail.com>
2022-12-15 14:33:27 +0500

    first commit

---
+++ redux-tut-master/redux/src/components/Home.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function Home(props) {
+    return (
+        <div>
+           
+            <h1>Home Component</h1>
+            <div className="cart-wrapper">
+                <div className="img-wrapper item">
+                    <img style={{width:'40px',heoght:'160px'}} src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-13-pro-green-witb-202203?wid=172&hei=392&fmt=jpeg&qlt=95&.v=1644965775338" />
+                </div>
+                <div className="text-wrapper item">
+                    <span>
+                        I-Phone
+                    </span>
+                    <span>
+                        Price: $1000.00
+                    </span>
+                </div>
+                <div className="btn-wrapper item">
+                    <button 
+                    onClick={
+                        ()=>{props.addToCartHandler({pice:1000,name:'i phone 11'})}
+                        }>
+                        Add To Cart</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Home
